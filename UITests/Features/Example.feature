7ab0737201adb509d0c4533bd5b11aed9e1d3779
Venkat Rao <venkat.rao@rgare.com>
2020-02-04 12:17:59 -0600

    Example project

---
+++ UITests/Features/Example.feature
@@ -0,0 +1,5 @@
+﻿Feature: Example
+
+@print
+Scenario: Print in console
+Given I print "Hello World" in console
---
+++ UITests/StepDefinitions/ExampleStepDefinitions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace UITests.StepDefinitions
+{
+    [Binding]
+    public sealed class ExampleStepDefinitions
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext context;
+
+        public ExampleStepDefinitions(ScenarioContext injectedContext)
+        {
+            context = injectedContext;
+        }
+
+        [Given(@"I print ""(.*)"" in console")]
+        public void GivenIPrintInConsole(string message)
+        {
+            Console.WriteLine("Printing in console {0}", message);
+        }
+
+    }
+}
